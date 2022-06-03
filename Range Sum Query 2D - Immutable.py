6e0f065a34d76e17a0019d7f1be6ac999b2d523b
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-03 16:19:20 -0700

    Add files via upload

--- Range Sum Query 2D - Immutable.py
+++ Range Sum Query 2D - Immutable.py
@@ -12,7 +12,7 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         ROWS, COLS = len(matrix), len(matrix[0])
-        self.sumMat = [[0] * (COLS + 1) for c in range(COLS + 1)]
+        self.sumMat = [[0] * (COLS + 1) for r in range(ROWS + 1)]
         
         for r in range(ROWS):
             prefix = 0
@@ -22,11 +22,11 @@ class NumMatrix:
                 self.sumMat[r + 1][c + 1] = prefix + above
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        r1, c1, r2, c2 = r1 + 1, c1 + 1, r2+ 1, c2 + 1
-        bottomRight = self.sumMat[r2][c2]
-        above = self.sumMat[r1 - 1][c2]
-        left = self.sumMat[r2][c1 - 1]
-        topLeft = self.sumMat[r1 - 1][c1 - 1]
+        row1, col1, row2, col2 = row1 + 1, col1 + 1, row2+ 1, col2 + 1
+        bottomRight = self.sumMat[row2][col2]
+        above = self.sumMat[row1 - 1][col2]
+        left = self.sumMat[row2][col1 - 1]
+        topLeft = self.sumMat[row1 - 1][col1 - 1]
         
         return bottomRight - above - left + topLeft
 
